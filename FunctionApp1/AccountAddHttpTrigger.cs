a9e2e20de39a0ca383db25654f19f99a3c03b3bf
mattruma <mattruma@gmail.com>
2019-12-19 06:24:14 -0500

    cleaned up comments

--- FunctionApp1/AccountAddHttpTrigger.cs
+++ FunctionApp1/AccountAddHttpTrigger.cs
@@ -11,11 +11,6 @@ using System.Threading.Tasks;
 
 namespace FunctionApp1
 {
-    // https://docs.microsoft.com/en-us/azure/logic-apps/logic-apps-http-endpoint
-    // https://marcelzehner.ch/2019/02/05/issues-with-creating-new-cosmos-db-documents-with-logic-app/
-    // https://docs.microsoft.com/en-us/azure/logic-apps/manage-logic-apps-with-visual-studio
-    // https://marketplace.visualstudio.com/items?itemName=VinaySinghMSFT.AzureLogicAppsToolsForVS2019
-
     public class AccountAddHttpTrigger
     {
         private IAccountDataStore _accountDataStore;
